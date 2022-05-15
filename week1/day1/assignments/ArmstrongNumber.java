ef9247099045e93c5be8c256eae88bb47b4fe3d4
Vaishali0106 <105578812+Vaishali0106@users.noreply.github.com>
2022-05-15 16:24:48 +0530

    Update ArmstrongNumber.java

--- week1/day1/assignments/ArmstrongNumber.java
+++ week1/day1/assignments/ArmstrongNumber.java
@@ -46,7 +46,22 @@ public class ArmstrongNumber {
 
 		
 
-		// Declare your input
+		int num=122;
+		int original=num;
+		int cal=0;
+
+		while(original>0){
+			int remainder=original%10;
+			cal=cal+(remainder*remainder*remainder);
+			original=original/10;
+
+		}
+		System.out.println(cal);
+		if(cal==num) {
+			System.out.println("Number is Armstrong");
+		}
+		else
+			System.out.println("Not Armstrong");// Declare your input
 
 		
 
